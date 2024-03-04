2d1f2e2ee04ffde7d5868a8c67e1b0f41f140cde
Šimun <simun.hordov5@gmail.com>
2024-03-04 16:51:27 +0100

    wrote the id for the tbody and wrote the fetchData function that captures data from data.json using ajax that sends an http request to retrieve the data and wrote the function to display the data in the tbody

---
+++ script.js
@@ -0,0 +1,48 @@
+let currentPage = 1;
+let itemsPerPage = 24;
+let data = [];
+
+const tbody = document.getElementById("table-body");
+const prevBtn = document.getElementById("prethodni");
+const firstBtn = document.getElementById("prvi");
+const showAllBtn = document.getElementById("prikazi-sve");
+const lastBtn = document.getElementById("zadnji");
+const nextBtn = document.getElementById("sljedeći");
+
+function fetchData() {
+  var xml = new XMLHttpRequest();
+  xml.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var data = JSON.parse(this.responseText);
+      displayData(data);
+    }
+  };
+  xml.open("GET", "data.json", true);
+  xml.send();
+}
+
+function displayData(data, currentPage, itemsPerPage) {
+  const tbody = document.getElementById("table-body");
+  tbody.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    if (
+      i >= (currentPage - 1) * itemsPerPage &&
+      i < currentPage * itemsPerPage
+    ) {
+      const tr = document.createElement("tr");
+
+      tr.innerHTML = `
+            <td>${item.rowId}</td>
+            <td>${item.iskaznica}</td>          
+            <td>${item.datum}</td>
+            <td>${item.vrijeme}</td>
+            <td>${item.oznaciti}</td>
+            <td>${item.barcode}</td>
+            <td><input type="text" value="${data[i].tezina}"></td>
+            <td>${item.registracija}</td>
+            `;
+
+      tbody.appendChild(tr);
+    }
+  }
+}
